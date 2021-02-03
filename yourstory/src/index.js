371e954bd0bd436fd1a925f90ae7f640bc23d38a
Masai School <bkjagdevcoder@gmail.com>
2021-02-03 12:20:35 +0530

    Redux Backend for all the Search Page

--- yourstory/src/index.js
+++ yourstory/src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import {Provider} from 'react-redux'
+import store from './Redux/store';
 import { BrowserRouter } from 'react-router-dom';
 
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
+    rootElement
 );
-
 
