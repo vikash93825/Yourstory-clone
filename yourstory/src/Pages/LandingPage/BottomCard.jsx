445d8362632532ab04c7d8aae3bbad5a2ca92ee6
Aloknath-sah <kumaraditya6396@gmail.com>
2021-02-04 20:50:19 +0530

    added comments and removed unnessesary code

--- yourstory/src/Pages/LandingPage/BottomCard.jsx
+++ yourstory/src/Pages/LandingPage/BottomCard.jsx
@@ -1,84 +1,9 @@
-// import React, {useEffect} from 'react';
-// import { makeStyles } from "@material-ui/core/styles";
-// import styles from './LandingPage.module.css'
-// import {CardComponent} from './CardComponent'
-// import {useSelector, useDispatch} from 'react-redux'
-// import {fetchSourceData} from '../../Redux/DataRedux/actionCreator'
-// import {
-//     Card,
-//     CardActionArea,
-//     CardActions,
-//     CardContent,
-//     CardMedia,
-//     Button,
-//     Typography,
-//     Grid,
-// } from "@material-ui/core";
-
-// const useStyles = makeStyles({
-//     root: {
-//         maxWidth: 650,
-        
-//     },
-//     right:{
-//         paddingRight: 100
-//     },
-//     media: {
-//         height: 340,
-//         borderRadius: 10
-//     },
-//     main: {
-//         paddingLeft: 120,
-//         paddingTop:70
-//     },
-// });
-
-// const BottomCard = () => {
-//     const dispatch = useDispatch()
-    
-//     useEffect(() => {
-//         dispatch(fetchSourceData())
-//         //dispatch(categoryParams())
-//     }, []);
-//     const source = useSelector(state => state.source)
-//     console.log(source)
-//     const startup = source.filter((item)=> item.category == "Startup")
-//     console.log(startup)
-//     //console.log(img_url)
-//     const classes = useStyles();
-//     return (
-        
-//         <Grid>
-//             <div className={styles.stcard} >
-//                 <img src="https://images.yourstory.com/assets/icons/ic_startup.svg" width="50px" />
-//                 <div>
-//                     <div className={styles.head} >Startup</div>
-//                     <div>Deep-dive stories of changemakers and innovators from the startup ecosystem</div>
-//                 </div>
-//             </div>
-//             <Grid item container>
-                
-//                 {
-//                     startup.map((item)=> <CardComponent {...item} /> )
-//                 }
-                
-//             </Grid>
-//         </Grid>
-//     );
-// }
-
-
-
-import ReactDOM from "react-dom";
 import Carousel from "react-elastic-carousel";
 import Item from "./Item";
 import React, {useEffect} from 'react';
-import { makeStyles } from "@material-ui/core/styles";
-import styles from './LandingPage.module.css'
 import {CardComponent} from './CardComponent'
 import {useSelector, useDispatch} from 'react-redux'
 import {fetchSourceData} from '../../Redux/DataRedux/actionCreator'
-//import "./styles.css";
 
 const breakPoints = [
     { width: 1, itemsToShow: 1 },
@@ -90,25 +15,28 @@ const breakPoints = [
 export const BottomCard=()=> {
     const dispatch = useDispatch()
     
+    //loading the source data so that different data can be shown using filter
     useEffect(() => {
         dispatch(fetchSourceData())
-        //dispatch(categoryParams())
     }, []);
+
+    //getting source data from database
     const source = useSelector(state => state.source)
-    console.log(source)
+
+    //filtering the startup data which will be used to show in the landing page
     const startup = source.filter((item)=> item.category == "Startup")
-    console.log(startup)
 
+    //filtering the unionBudget data which will be used to show in the landing page
     const unionBudget = source.filter((item)=> item.category == "unionBudget")
-    console.log(unionBudget)
 
+    //filtering the latestStory data which will be used to show in the landing page
     const latestStory = source.filter((item)=> item.category == "latestStory")
-    console.log(latestStory)
 
+    //filtering the interview data which will be used to show in the landing page
     const interview = source.filter((item)=> item.category == "interview")
-    console.log(interview)
     return (
         <div>
+            {/* showing the startup data by mapping in the carousal */}
             <Carousel breakPoints={breakPoints}>
             {
                 startup.map((item)=> (
@@ -118,6 +46,8 @@ export const BottomCard=()=> {
                 ) )
             }
             </Carousel>
+
+            {/* showing the unionBudget data by mapping in the carousal */}
             <Carousel breakPoints={breakPoints}>
             {
                 unionBudget.map((item)=> (
@@ -127,6 +57,8 @@ export const BottomCard=()=> {
                 ) )
             }
             </Carousel>
+
+            {/* showing the latestStory data by mapping in the carousal */}
             <Carousel breakPoints={breakPoints}>
             {
                 latestStory.map((item)=> (
@@ -136,6 +68,8 @@ export const BottomCard=()=> {
                 ) )
             }
             </Carousel>
+
+            {/* showing the interview data by mapping in the carousal */}
             <Carousel breakPoints={breakPoints}>
             {
                 interview.map((item)=> (
